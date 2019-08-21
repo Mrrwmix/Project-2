3d6e85a08261b89eb7418b4c7cd2f815bcc31442
Matthew Winemiller <seidoc@gmail.com>
2019-08-20 21:51:45 -0700

    Testing force change

--- server.js
+++ server.js
@@ -1,54 +1,54 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
-var db = require("./models");
+require('dotenv').config();
+var express = require('express');
+var exphbs = require('express-handlebars');
+var db = require('./models');
 //var seeds = require("./test/seeds");
 var app = express();
 var PORT = process.env.PORT || 3000;
-var passport = require("passport");
-var session = require("express-session");
-var bodyParser = require("body-parser");
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(express.static("semantic"));
+app.use(express.static('public'));
+app.use(express.static('semantic'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({ secret: "buddyguy", resave: true, saveUninitialized: true }));
+app.use(session({ secret: 'buddyguy', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 // Handlebars
 app.engine(
-  "handlebars",
+  'handlebars',
   exphbs({
-    defaultLayout: "main",
-    partialsDir: __dirname + "/views/partials/"
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
   })
 );
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 //Passport strategies
-require("./config/passport/passport")(passport, db.Users);
+require('./config/passport/passport')(passport, db.Users);
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+if (process.env.NODE_ENV === 'test') {
+  syncOptions.force = false;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
       PORT,
       PORT
     );
